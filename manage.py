8c29e8a65f714acb35da0e4df25ad6c59cfb7c59
Wen-Jian <genning7@gmail.com>
2022-04-06 15:12:11 +0800

    chore: don't use flase script

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 from distutils.log import debug
-from flask_script import Manager, Server
+# from flask_script import Manager, Server
 import os
 import sys
 sys.path.append(os.path.join(os.getcwd(), 'app/'))
@@ -7,12 +7,13 @@ from app import main
 from app.models.user import User
 from waitress import serve
 
-manager = Manager(main)
-manager.add_command('runserver', serve(main, port=5000))
+# manager = Manager(main)
+# manager.add_command('runserver', serve(main, port=5000))
 
-@manager.shell
-def make_shell_context():
-    return dict(app=main, User=User)
+# @manager.shell
+# def make_shell_context():
+#     return dict(app=main, User=User)
 
 if __name__ == '__main__':
-    manager.run()
+    # manager.run()
+    serve(main, port=5000)
